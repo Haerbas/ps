cfb3240b3627c767005d397873b2fbe57087c84f
Haerbas <hakan@erbas.co>
2022-03-17 16:42:41 +0100

    .

--- basics.ps1
+++ basics.ps1
@@ -1,3 +1,82 @@
+Get-ADUser -Identity "hrb" -Properties proxyAddresses | ft -Wrap -AutoSize
+Get-Command -Name Get-Team
+Find-Module -Name *Team*
+Get-Module -Name MicrosoftTeams -ListAvailable
+Install-Module -Name MicrosoftTeams -Scope CurrentUser
+Connect-MicrosoftTeams
+get-team | Sort-Object name
+
+Test-Connection 127.0.0.1 | ft -Wrap -AutoSize
+
+get-childitem
+
+$Variable = "Test"
+
+Write-Host $Variable -ForegroundColor Green
+
+get-location
+
+Get-Process
+get-help Start-Process -Examples
+((get-volume)[0]).SizeRemaining
+
+$colours = @()
+$colours += "black"
+$colours += "white"
+$colours += "green"
+$colours += "blue"
+$colours += "purple"
+$colours += "dark"
+
+$colours.count
+
+for ($i = 0; $i -le $colours.Length; $i++){write-host "Das sind hier die Werte: " $colours[$i]}
+
+get-help if -Examples
+
+do
+{
+$i = 0
+write-host = "Das sind hier die Werte $colours[$colours]"
+$i++
+}while ($i -le $colours.Length)
+
+foreach ($color in $colours)
+{ write-host "Das ist hier der Wert $color"}
+
+
+$proc = Get-Process
+
+$proc.length
+
+for ($i=0; $i -le $proc.Length; $i++)
+{
+write-host "Prozesse: " $proc[$i]
+}
+
+[int]$var=Read-Host "Bitte gebe eine Zahl ein"
+
+if ($var -eq 34234)
+{
+write-host "die Zahl ist $var" -ForegroundColor Cyan
+}
+else
+{
+write-host "ich weiß es nicht!" -ForegroundColor DarkGreen
+}
+
+Get-Location
+
+set-location -Path C:\Users\hrb\Downloads
+Get-ChildItem -Path *.txt | ? {$_.Length -gt 80} | Sort-Object -Property Length -Descending | Select-Object Name, Length
+
+new-item -ItemType File -Name "IchbineinFile.txt"
+get-childitem | Sort-Object -Property LastWriteTime
+
+
+
+
+
 Get-ChildItem | ForEach-Object { $_ } | Where-Object | 
 
 get-help Where-Object -Examples
@@ -23,3 +102,8 @@ if (condition) {
 }else {
     
 }
+
+
+#read content from file, replace it with the method and write it back
+$content = [System.IO.File]::ReadAllText("C:\Users\hrb\AppData\Local\NDI\NDI_Bridge\ndi_bridge_vpn.ndibridge").Replace("Quality","25",1)
+[System.IO.File]::WriteAllText("c:\bla.txt", $content)
--- wednesday.ps1
+++ wednesday.ps1
@@ -1,10 +1,10 @@
 #generate Input Sources with this Parameters
-$frn = @(30000,25000)
-$frd = @(1000,1200)
-$name = @("Bild","Eingabe")
-$image = @("C:\temp\test\Image1_1080.png","C:\temp\test\Image2_720.png")
+$frn = @(30000,25000,60000)
+$frd = @(1000,1200,1001)
+$name = @("Bild","Eingabe","4K")
+$image = @("C:\temp\test\Image1_1080.png","C:\temp\test\Image2_720.png","C:\temp\test\Image1_2160.png")
 
-Start-Sleep -Seconds 15
+
 
 
 Enable-PSRemoting
@@ -12,29 +12,41 @@ Set-Location "c:\temp\test"
 #Start Remote Call to the Analyze computer and set the Client as TrustedHosts
 Set-Item WSMan:\localhost\Client\TrustedHosts -value "*" -Force
 
-#Query for the running processes and starting them
+
 $session = New-PSSession -ComputerName 10.0.1.20 -ErrorAction Stop
-Invoke-Command -Session $session -ScriptBlock {
-if(!(Get-Process | Where-Object { $_.ProcessName -eq "NDI Discovery Service" }))
+Invoke-Command -Session $session -ScriptBlock { 
+
+if(!(Get-Process | Where-Object { $_.ProcessName -eq "Application.NDI.VPN.UI" }))
 {
-    Start-process "C:\Program Files\NDI\NDI 5 Tools\Discovery\NDI Discovery Service.exe"
-}
-if(!(Get-Process | Where-Object { $_.ProcessName -eq "*Application.NDI.VPN.UI" }))
-{
-    Start-process "C:\Program Files\NDI\NDI 5 Tools\Bridge\Application.NDI.VPN.UI.exe" /host
+    Start-process "C:\Program Files\NDI\NDI 5 Tools\Bridge\Application.NDI.VPN.UI.exe" /host -WindowStyle Normal
 }
 if(!(Get-Process | Where-Object { $_.ProcessName -eq "Application.Network.StudioMonitor.x64" }))
 {
-    Start-process "C:\Program Files\NDI\NDI 5 Tools\Studio Monitor\Application.Network.StudioMonitor.x64.exe" }
+    Start-process "C:\Program Files\NDI\NDI 5 Tools\Studio Monitor\Application.Network.StudioMonitor.x64.exe" -WindowStyle Normal
 }
+if(!(Get-Process | Where-Object { $_.ProcessName -like "*Discovery*" }))
+{
+   Start-process "C:\Program Files\NDI\NDI 5 Tools\Discovery\NDI Discovery Service.exe"
+}}
+
+$session1 = New-PSSession -ComputerName 10.0.2.10 -ErrorAction Stop
+Invoke-Command -Session $session1 -ScriptBlock {
+
+if(!(Get-Process | Where-Object { $_.ProcessName -eq "Application.NDI.VPN.UI" }))
+{
+    Start-process "C:\Program Files\NDI\NDI 5 Tools\Bridge\Application.NDI.VPN.UI.exe" /host -WindowStyle Normal
+}}
+
 
 #Run Command as length of the Array
 for ($i = 0; $i -lt $frn.count; $i++){
-start NDIlib_Send_PNG.x64.exe "/frn:$($frn[$i]) /frd:$($frd[$i]) /name:$($name[$i]) /image:$($image[$i])"
+Start NDIlib_Send_PNG.x64.exe "/frn:$($frn[$i]) /frd:$($frd[$i]) /name:$($name[$i]) /image:$($image[$i])"
+Write-Output "/frn:$($frn[$i]) /frd:$($frd[$i]) /name:$($name[$i]) /image:$($image[$i])"
+
 #First Command looks for the Sources and saves it in a file
-Invoke-Command -ComputerName 10.0.2.20 -ScriptBlock {& "C:\Program Files\NDI\NDI 5 Analysis BETA\x64\NDIAnalysis.exe" /find > "c:\temp\hakan.txt"; 
+Invoke-Command -ComputerName 10.0.2.20 -ScriptBlock {& "C:\Program Files\NDI\NDI 5 Analysis BETA\x64\NDIAnalysis.exe" /find > "c:\temp\analysis.txt"; 
 #Second Command takes only the necessary part with source name, and saves it into a file
-Select-String -Path c:\temp\hakan.txt -Pattern "EC2AMAZ-SOURCE" | foreach {($_.Line)} | Out-String > c:\temp\string.txt; 
+Select-String -Path c:\temp\analysis.txt -Pattern "EC2AMAZ-" | foreach {($_.Line)} | Out-String > c:\temp\string.txt; 
 #Reading File content and format/split it in the desired form and save it also in a file
 $file = get-content "c:\temp\string.txt"
 $file | foreach { $source = $_ -split '=' ; $newfile = $source[1] | out-file -Append C:\temp\source.txt } 
@@ -50,6 +62,8 @@ If(!(test-path $path))
 }
 #Foreach as the value of the sources
 foreach ($x in $newsource){& "C:\Program Files\NDI\NDI 5 Analysis BETA\x64\NDIAnalysis.exe" /source:$x /csv:$path\$Type-$(get-date -Format "MMddyy.HHmmss")-$x.csv /time:10 }
+Remove-Item c:\temp\*.txt -force
+Remove-Item $path\*ANALYZE*.csv
 }
 Start-Sleep -Seconds 5
 get-Process | ? {$_.ProcessName -like "*NDIlib*"} | Stop-Process -Force
